59e6faa3427535fa8c4339507f8c14ed6380b350
renyh <1365305148@qq.com>
2025-09-19 14:11:19 +0800

    feat: gemini模型调用

--- gemini_model/gemini_flash.py
+++ gemini_model/gemini_flash.py
@@ -1,13 +1,33 @@
 from google import genai
+from PIL import Image
+from io import BytesIO
 
-if __name__ == "__main__":
-    client = genai.Client(api_key="AIzaSyD9iAUKqNHWerRAQJSpmcq-cGelmX9AtPs")
+client = genai.Client(api_key="AIzaSyD9iAUKqNHWerRAQJSpmcq-cGelmX9AtPs")
 
-    cur_file = client.files.upload(file="C:\\Users\\Admin\\Downloads\\a.mp4")
 
+def generate_image(prompt):
     response = client.models.generate_content(
-        model="gemini-2.5-flash", contents=[cur_file, "Summarize this video. Then create a quiz with an answer key based on the information in this video."]
+        model="gemini-2.5-flash-image-preview",
+        contents=[prompt],
+    )
+
+    for part in response.candidates[0].content.parts:
+        if part.text is not None:
+            print(part.text)
+        elif part.inline_data is not None:
+            image = Image.open(BytesIO(part.inline_data.data))
+            image.save("generated_image.png")
+
+
+def chat_with_gemini(prompt):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=[prompt]
     )
 
     print(response.text)
 
+
+if __name__ == "__main__":
+    # generate_image("Create a picture of a nano banana dish in a fancy restaurant with a Gemini theme")
+
+    chat_with_gemini("你知道西游记这本书吗? 大概介绍一下，200字左右吧。")
